1e22b6e3453d5fe902427badc7b02a4799585ccc
iludart <peter.penev@hotamail.com>
2018-03-24 17:15:31 +0200

    Added basic header navigation

--- EducationSystemUI/src/components/HomePage.js
+++ EducationSystemUI/src/components/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeaderNavigation from './header/HeaderNavigation';
 
 const HomePage = () => (
   <div>
+    <HeaderNavigation />
     Home <Link to='/isLogged'>Check User</Link>
   </div>
 );
