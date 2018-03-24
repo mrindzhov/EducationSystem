fa2caf208648bad468cd822475fe049a4673d20a
iludart <peter.penev@hotamail.com>
2018-03-24 14:51:53 +0200

    Modified git ignore + added react ui separate project

---
+++ EducationSystem/EducationSystem.UI/components/App.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import ReactDOM from "react-dom";
+
+export default class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
