249d372d3782d419abc92cc73f6a7487f244afc1
RBkaTa <qvor007@abv.bg>
2018-03-24 20:40:19 +0200

    Updated project controller.

---
+++ EducationSystem/EducationSystem.Models/CreateProjectDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationSystem.Models
+{
+    public class CreateProjectDTO
+    {
+        public string Name { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public string Requirements { get; set; }
+
+        public ICollection<Skill> SkillsNeeded { get; set; }
+    }
+}
