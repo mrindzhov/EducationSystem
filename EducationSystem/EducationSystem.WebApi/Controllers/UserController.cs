5f46ec5892426bda3e67fe3304f2667160631d55
asenvolev <asen_volev_gh@abv.bg>
2018-03-25 12:28:02 +0300

    Added Decline Request, Send Request, GetIdByUsername

---
+++ EducationSystem/EducationSystem.WebApi/Controllers/UserController.cs
@@ -0,0 +1,115 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+using EducationSystem.Services;
+
+namespace EducationSystem.WebApi.Controllers
+{
+    //[Authorize]
+    [EnableCors("*", "*", "*")]
+    public class UserController : ApiController
+    {
+        public IHttpActionResult GetByUsername(string username)
+        {
+            var service = new UserService();
+            var user = service.GetByUsername(username);
+
+            return Json(user);
+        }
+
+        public IHttpActionResult GetIdByUsername(string username)
+        {
+            var service = new UserService();
+            string user;
+            try
+            {
+                user = service.GetIdByUsername(username);
+            }
+            catch (System.Exception ex)
+            {
+                return NotFound();
+            }
+            return Json(user);
+        }
+
+        public IHttpActionResult GetAllUsersBySkill(int skillType, double minumRank)
+        {
+            var service = new UserService();
+            var users = service.GetAllUsersBySkill(skillType, minumRank);
+
+            return Json(users);
+        }
+
+        public IHttpActionResult GetParticipants(int projectId)
+        {
+            var service = new UserService();
+            var participants = service.GetParticipants(projectId);
+
+            return Json(participants);
+        }
+
+        public IHttpActionResult GetReceivedRequests(int projectId)
+        {
+            var service = new UserService();
+            var receivedRequests = service.GetReceivedRequests(projectId);
+
+            return Json(receivedRequests);
+        }
+
+        public IHttpActionResult GetRequestedDevelopers(int projectId)
+        {
+            var service = new UserService();
+            var requestedDevelopers = service.GetRequestedDevelopers(projectId);
+
+            return Json(requestedDevelopers);
+        }
+
+        public IHttpActionResult AcceptProject(int projectId, string username)
+        {
+            var service = new UserService();
+            try
+            {
+                service.AcceptProject(projectId, username);
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult DeclineProject(int projectId, string username)
+        {
+            var service = new UserService();
+            try
+            {
+                service.DeclineProject(projectId, username);
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        public IHttpActionResult SendRequestToProject(string username, int projectId)
+        {
+            var service = new UserService();
+            try
+            {
+                service.SendRequestToProject(username,projectId);
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        //public IHttpActionResult AddSkill(int skillNum)
+        //{
+        //    var service = new UserService();
+        //    service.AddSkill(skillNum);
+        //}
+
+    }
+}
